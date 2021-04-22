0cc8a465ad765ddd3a9ef1222dd78ec3134dfe78
Juan Carlos Madrid Flores <juancarlos17.mf@gmail.com>
2021-04-22 18:48:50 -0500

    commit

---
+++ Semana_05/index.js
@@ -0,0 +1,6 @@
+//con js puro:
+//const { Server } = require("./config/Server");
+//con babelrc
+import { Server } from "./config/Server";
+const objServer = new Server();
+objServer.iniciarServidor(); 
