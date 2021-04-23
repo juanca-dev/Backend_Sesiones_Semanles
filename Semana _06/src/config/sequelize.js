bc24f551d6a6b94102b837abd4e367eff3f94cd6
Juan Carlos Madrid Flores <juancarlos17.mf@gmail.com>
2021-04-22 23:36:10 -0500

    update

--- Semana _06/src/config/sequelize.js
+++ Semana _06/src/config/sequelize.js
@@ -1,15 +1,17 @@
 import { Sequelize } from "sequelize";
 
 export const conexion = new Sequelize(
-  "almacen", //database name
-  "root",   //username
-  "root",   //contraseña
-{
-    dialect: "mysql",
+  "almacen", // database name
+  "root", // username
+  "root", // password
+  {
+    dialect: "mysql", // tambien podemos usar dialectos para pgadmin, sqlserver, sqlite3, mariadb, mysql
+    // /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */ para sqlite se usa connection URI
     host: "127.0.0.1",
-    timezone: "-05:00",
+    port: 3306,
+    timezone: "-05:00", // no funciona en SQLITE
     dialectOptions: {
-
+      // sirve para que al momento de mostrar las fechas, automaticamente las convierta en string y no tener que hacer una conversion manual
       dateStrings: true,
     },
   }
--- Semana _06/src/index.js
+++ Semana _06/src/index.js
@@ -1,3 +1,5 @@
+
 import Server from "./config/server";
+
 const objServer = new Server();
 objServer.start();
